264e2768a9150e3b408e929dc695d8e869fefb73
kingosticks <kingosticks@gmail.com>
2014-02-24 23:55:06 +0000

    Update the package name capitalisation to match

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_version(filename):
 
 
 setup(
-    name='Mopidy-Tunein',
+    name='Mopidy-TuneIn',
     version=get_version('mopidy_tunein/__init__.py'),
     url='https://github.com/kingosticks/mopidy-tunein',
     license='Apache License, Version 2.0',
